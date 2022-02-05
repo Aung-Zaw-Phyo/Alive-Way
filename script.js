68d50709454bc5337bb2d8720322314ba991a141
Aung Zaw Phyo <aungzawphyo1102@gmail.com>
2022-02-05 21:48:35 +0900

    adding menubars on every pages

---
+++ script.js
@@ -0,0 +1,22 @@
+const mubarTag = document.querySelector(".mubar");
+const line1Tag = document.querySelector(".line1");
+const line2Tag = document.querySelector(".line2");
+const line3Tag = document.querySelector(".line3");
+const overlayTag = document.querySelector(".overlay");
+
+
+mubarTag.addEventListener("click", ()=>{
+    if (mubarTag.classList.contains("opened")) {
+        line2Tag.classList.remove("remove")
+        line1Tag.classList.remove("line1Deg")
+        line3Tag.classList.remove("line3Deg")
+         mubarTag.classList.remove("opened")
+        overlayTag.classList.remove("d-block")
+    } else {
+        line2Tag.classList.add("remove")
+        line1Tag.classList.add("line1Deg")
+        line3Tag.classList.add("line3Deg")
+        mubarTag.classList.add("opened")
+        overlayTag.classList.add("d-block")
+    }
+})
